458d775757fc4a52e33e657aadf252d18285b963
Clayton <claystone@gmail.com>
2026-09-25 17:24:05 +0000

    chore(android): bump version to 1.1.3-clay.10

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.uzairansar.hermex"
         minSdk = 26
         targetSdk = 36
-        versionCode = 42
-        versionName = "1.1.3-clay.9"
+        versionCode = 43
+        versionName = "1.1.3-clay.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
